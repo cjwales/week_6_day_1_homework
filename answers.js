76381348a34bfa693f496bde9a09ea2a66c4f93f
cjwales <cjwales@gmail.com>
2019-09-30 22:25:38 +0100

    format change

--- answers.js
+++ answers.js
@@ -1,179 +1,179 @@
 //Episode 1
-// const scenario = {
-//   murderer: 'Miss Scarlet',
-//   room: 'Library',
-//   weapon: 'Rope'
-// };
-//
-// const declareMurderer = function() {
-//   return `The murderer is ${scenario.murderer}.`;
-// }
-//
-// const verdict = declareMurderer();
-// console.log(verdict);
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
 
 //Will print "The murderer is Miss Scarlet" because the object, function and variable are all const.
 
 
 //Episode 2
-// const murderer = 'Professor Plum';
-//
-// const changeMurderer = function() {
-//   murderer = 'Mrs. Peacock';
-// }
-//
-// const declareMurderer = function() {
-//   return `The murderer is ${murderer}.`;
-// }
-//
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
 
 // This will not run as line 25 already declares declareMurderer.
 
 //Episode 3
-// let murderer = 'Professor Plum';
-//
-// const declareMurderer = function() {
-//   let murderer = 'Mrs. Peacock';
-//   return `The murderer is ${murderer}.`;
-// }
-//
-// const firstVerdict = declareMurderer();
-// console.log('First Verdict: ', firstVerdict);
-//
-// const secondVerdict = `The murderer is ${murderer}.`;
-// console.log('Second Verdict: ', secondVerdict);
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
 
 // The first log will output First Murderer is Mrs. Peacock because the variable murderer is assigned inside the function.
 //The second log will output The murderer is Prof Plum because it doesnt have access to the variable on line 39.
 
 //Episode 4
-// let suspectOne = 'Miss Scarlet';
-// let suspectTwo = 'Professor Plum';
-// let suspectThree = 'Mrs. Peacock';
-//
-// const declareAllSuspects = function() {
-//   let suspectThree = 'Colonel Mustard';
-//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
-// }
-//
-// const suspects = declareAllSuspects();
-// console.log(suspects);
-// console.log(`Suspect three is ${suspectThree}.`);
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
 
 //the first log will return 'The suspeccts are Miss Scarlet, Professor Plum, Colonel Mustard.', because the let on line 58 changes suspectThree to Colonel Mustard.
 //The second log will return 'Suspect three is Mrs. Peacock.', because of the let on line 58.
 
 //Episode 5
-// const scenario = {
-//   murderer: 'Miss Scarlet',
-//   room: 'Kitchen',
-//   weapon: 'Candle Stick'
-// };
-//
-// const changeWeapon = function(newWeapon) {
-//   scenario.weapon = newWeapon;
-// }
-//
-// const declareWeapon = function() {
-//   return `The weapon is the ${scenario.weapon}.`;
-// }
-//
-// changeWeapon('Revolver');
-// const verdict = declareWeapon();
-// console.log(verdict);
-//This will return 'The weapon is the Revolver', because line 77 changes the weapon string from Candle Stick to Revolver.
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Kitchen',
+  weapon: 'Candle Stick'
+};
+
+const changeWeapon = function(newWeapon) {
+  scenario.weapon = newWeapon;
+}
+
+const declareWeapon = function() {
+  return `The weapon is the ${scenario.weapon}.`;
+}
+
+changeWeapon('Revolver');
+const verdict = declareWeapon();
+console.log(verdict);
+// This will return 'The weapon is the Revolver', because line 77 changes the weapon string from Candle Stick to Revolver.
 
 //Episode 6
-// let murderer = 'Colonel Mustard';
-//
-// const changeMurderer = function() {
-//   murderer = 'Mr. Green';
-//
-//   const plotTwist = function() {
-//     murderer = 'Mrs. White';
-//   }
-//
-//   plotTwist();
-// }
-//
-// const declareMurderer = function () {
-//   return `The murderer is ${murderer}.`;
-// }
-//
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
 
 // 'The murderer is Mrs. White', as the plotTwist changes the value of murderer to Mrs. White.
 
 //Episode 7
-// let murderer = 'Professor Plum';
-//
-// const changeMurderer = function() {
-//   murderer = 'Mr. Green';
-//
-//   const plotTwist = function() {
-//     let murderer = 'Colonel Mustard';
-//
-//     const unexpectedOutcome = function() {
-//       murderer = 'Miss Scarlet';
-//     }
-//
-//     unexpectedOutcome();
-//   }
-//
-//   plotTwist();
-// }
-//
-// const declareMurderer = function() {
-//   return `The murderer is ${murderer}.`;
-// }
-//
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
+let murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
+
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
+
+    unexpectedOutcome();
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
 
 //'The murderer is Mr Green', as line 135 resets it back to Mr Green.
 
 //Episode 8
 
-// const scenario = {
-//   murderer: 'Mrs. Peacock',
-//   room: 'Conservatory',
-//   weapon: 'Lead Pipe'
-// };
-//
-// const changeScenario = function() {
-//   scenario.murderer = 'Mrs. Peacock';
-//   scenario.room = 'Dining Room';
-//
-//   const plotTwist = function(room) {
-//     if (scenario.room === room) {
-//       scenario.murderer = 'Colonel Mustard';
-//     }
-//
-//     const unexpectedOutcome = function(murderer) {
-//       if (scenario.murderer === murderer) {
-//         scenario.weapon = 'Candle Stick';
-//       }
-//     }
-//
-//     unexpectedOutcome('Colonel Mustard');
-//   }
-//
-//   plotTwist('Dining Room');
-// }
-//
-// const declareWeapon = function() {
-//   return `The weapon is ${scenario.weapon}.`
-// }
-//
-// changeScenario();
-// const verdict = declareWeapon();
-// console.log(verdict);
+const scenario = {
+  murderer: 'Mrs. Peacock',
+  room: 'Conservatory',
+  weapon: 'Lead Pipe'
+};
+
+const changeScenario = function() {
+  scenario.murderer = 'Mrs. Peacock';
+  scenario.room = 'Dining Room';
+
+  const plotTwist = function(room) {
+    if (scenario.room === room) {
+      scenario.murderer = 'Colonel Mustard';
+    }
+
+    const unexpectedOutcome = function(murderer) {
+      if (scenario.murderer === murderer) {
+        scenario.weapon = 'Candle Stick';
+      }
+    }
+
+    unexpectedOutcome('Colonel Mustard');
+  }
+
+  plotTwist('Dining Room');
+}
+
+const declareWeapon = function() {
+  return `The weapon is ${scenario.weapon}.`
+}
+
+changeScenario();
+const verdict = declareWeapon();
+console.log(verdict);
 
 //'The weapon is Candle Stick', because unexpectedOutcome changes it to Candle Stick
 
